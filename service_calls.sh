bc93f19389fcf110e69c0868a7a1f72e5b51180a
naveen-motorai <naveen.datha@motor-ai.com>
2022-11-23 14:37:52 +0100

    list of service calls for sync.

---
+++ service_calls.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# To disable CAN output
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "output_control_target_list_can", value: 0, sensor_id: 107}"
+
+# To set sync. master
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "sync_mode", value: 1, sensor_id: 107}"
+
+# To set no. of sync slaves in group-1 (only to master)
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "sync_nof_devices_1st_group", value: 1, sensor_id: 107}"
+
+# To set no. of sync slaves in group-2 (only to master)
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "sync_nof_devices_2nd_group", value: 0, sensor_id: 107}"
+
+# To set no. of time sync. devices (only to master)
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "time_sync_nof_devices", value: 1, sensor_id: 107}"
+
+# To set sync. slaves
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "sync_mode", value: 2, sensor_id: 101}"
+
+# To set sync. slave id
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "sync_slave_identifier", value: 1, sensor_id: 101}"
+
+# To set sync. group identifier
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "sync_group_identifier", value: 0, sensor_id: 101}"
+
+# To set time sync
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "time_sync_mode", value: 1, sensor_id: 107}"
+ros2 service call /smartmicro_radar_node/set_radar_mode umrr_ros2_msgs/srv/SetMode "{param: "time_sync_mode", value: 2, sensor_id: 101}"
+
+# To set global UTC time (only to master) (run both commands below in order)
+ros2 service call /smartmicro_radar_node/set_time_to_sys umrr_ros2_msgs/srv/SetTimeToSys "{param: "comp_timebase_set_seconds_val", sensor_id: 101}"
+ros2 service call /smartmicro_radar_node/set_time_to_sys umrr_ros2_msgs/srv/SetTimeToSys "{param: "comp_timebase_set_frac_seconds_val", sensor_id: 101}"
